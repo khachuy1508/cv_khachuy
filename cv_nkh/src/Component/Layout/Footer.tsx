f949b11d3a79adcefa011de6bee2e112cd99cde5
Luke <khachuy.nguyen@gumiviet.com>
2022-08-23 13:34:14 +0700

    add link to icon

--- cv_nkh/src/Component/Layout/Footer.tsx
+++ cv_nkh/src/Component/Layout/Footer.tsx
@@ -1,16 +1,28 @@
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-import { IconButton } from "@mui/material";
+import { IconButton, Link } from "@mui/material";
 
 export const Footer: React.FC = () => {
   return (
     <div className="inline">
       <IconButton aria-label="facebook">
-        <FacebookIcon />
+        <a
+          href="https://www.facebook.com/khachuy158"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FacebookIcon />
+        </a>
       </IconButton>
       <IconButton aria-label="linked">
-        <LinkedInIcon />
+        <a
+          href="https://www.linkedin.com/in/khachuy158"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
       </IconButton>
     </div>
   );
